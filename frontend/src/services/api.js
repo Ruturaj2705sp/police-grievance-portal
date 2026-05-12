76b5e4170ad792384a6149ee6e2c164ba410ed72
Ruturaj2705sp <firstyr27@gmail.com>
2026-05-12 22:34:11 +0530

    Cleaned api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,3 @@
-/**
- * API Service
- * Axios instance with base configuration and interceptors
- */
 import axios from 'axios';
 
 const api = axios.create({
@@ -9,29 +5,3 @@ const api = axios.create({
 });
 
 export default api;
-
-// Request interceptor - attach token automatically
-api.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem('token');
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => Promise.reject(error)
-);
-
-// Response interceptor - handle global errors
-api.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    if (error.response?.status === 401) {
-      localStorage.removeItem('token');
-      window.location.href = '/login';
-    }
-    return Promise.reject(error);
-  }
-);
-
-
